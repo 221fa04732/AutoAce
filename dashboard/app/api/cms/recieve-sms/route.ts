83ff675e299a4443dff83fd70f8590b1b3feb86f
221fa04732 <221fa04732@gmail.com>
2026-01-09 22:18:28 +0530

    test logic to recieve sms message

--- dashboard/app/api/cms/recieve-sms/route.ts
+++ dashboard/app/api/cms/recieve-sms/route.ts
@@ -1,41 +1,46 @@
 import { NextRequest, NextResponse } from "next/server";
-import { createClient } from "@/lib/supabase/server";
-import { createClient as forAdmin} from '@supabase/supabase-js'
+// import { createClient } from "@/lib/supabase/server";
+// import { createClient as forAdmin} from '@supabase/supabase-js'
 
-const supabaseAdmin = forAdmin(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.SUPABASE_SERVICE_ROLE_KEY as string 
-)
+// const supabaseAdmin = forAdmin(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+//   process.env.SUPABASE_SERVICE_ROLE_KEY as string 
+// )
 
 export async function POST(req : NextRequest){
-    try{
-        const smsRecieveData = await req.json();
-        const supabase =await createClient()
+    // try{
+    //     const smsRecieveData = await req.json();
+    //     const supabase =await createClient()
 
-        const { data : user_data, error : user_error} = await supabase.auth.getUser();
+    //     const { data : user_data, error : user_error} = await supabase.auth.getUser();
         
-        if(user_error || !user_data || user_data.user.user_metadata.role != 'user'){
-            return NextResponse.json({
-                message : 'Unauthorized access'
-            },{status : 401})
-        }
+    //     if(user_error || !user_data || user_data.user.user_metadata.role != 'user'){
+    //         return NextResponse.json({
+    //             message : 'Unauthorized access'
+    //         },{status : 401})
+    //     }
 
-        const {error : message_error } = await supabaseAdmin.from('sms_message').insert({
-            conversationId : '1',
-            message : String(smsRecieveData),
-            sender : 'user',
-        })
-        if(message_error){throw new Error()}
+    //     const {error : message_error } = await supabaseAdmin.from('sms_message').insert({
+    //         conversationId : '1',
+    //         message : String(smsRecieveData),
+    //         sender : 'user',
+    //     })
+    //     if(message_error){throw new Error()}
         
-        return NextResponse.json({ 
-            success: true, 
-            message: "sms send successfully!" 
-        });
-    }
-    catch(err){
-        return NextResponse.json({
-            err,
-            message : 'Server error'
-        },{ status : 500})
-    }
+    //     return NextResponse.json({ 
+    //         success: true, 
+    //         message: "sms send successfully!" 
+    //     });
+    // }
+    // catch(err){
+    //     return NextResponse.json({
+    //         err,
+    //         message : 'Server error'
+    //     },{ status : 500})
+    // }
+
+    return NextResponse.json({ 
+        success: true, 
+        message: "sms send successfully!" 
+    }, {status : 200 });
 }   
